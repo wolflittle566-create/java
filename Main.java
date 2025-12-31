f068b5b3cb577652d981c661ad8380b0a24da70a
wolflittle566-create <wolflittle566@gmail.com>
2025-12-31 14:59:13 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello,World!");
+    public static void main (String[] args) {
+        String name="Демид";
+        int age =20;
+        double height =1.84;
+        boolean student= true;
+
+        System.out.println("Демид:" +name);
+        System.out.println("Возраст:" +age);
+        System.out.println("Рост:" + height);
+        System.out.println("Студент:" + student)  ;
     }
 }
