36dcd9067ffd8946607e33b2946fbf18c6824ae5
wolflittle566-create <wolflittle566@gmail.com>
2025-12-31 14:38:22 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Программа запущена");
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
     }
 }
